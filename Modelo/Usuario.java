c3c76f053e1bbee35a1e92a8090541582b1f203d
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-10-03 09:52:15 -0300

    Quite logistica en Usuario

--- Modelo/Usuario.java
+++ Modelo/Usuario.java
@@ -6,11 +6,6 @@ public class Usuario {
     private String nombre;
 
     public Usuario(String username, String contraseña, String nombre) {
-        if (username == null || username.isBlank() ||
-                contraseña == null || contraseña.isBlank() ||
-                nombre == null || nombre.isBlank()) {
-            throw new IllegalArgumentException("Se requieren datos.");
-        }
         this.username = username;
         this.contraseña = contraseña;
         this.nombre = nombre;
@@ -27,9 +22,6 @@ public class Usuario {
     }
 
     public void setUsername(String username) {
-        if (username == null || username.isBlank()) {
-            throw new IllegalArgumentException("Nuevo username no ingresado");
-        }
         this.username = username;
     }
 }
